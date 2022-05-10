619abd54aca48b70e7272f78dd881ef6f88f18fc
jyothibasai3 <jyothi.basai3@gmail.com>
2022-05-10 15:23:02 +0530

    MAVEN sample project

---
+++ src/test/java/MAVEN/CreateUserTest.java
@@ -0,0 +1,12 @@
+package MAVEN;
+
+import org.testng.annotations.Test;
+
+public class CreateUserTest {
+	@Test
+	public void CreateUsert()
+	{
+	     System.out.println("hi this is jyothi");
+	}
+
+}
